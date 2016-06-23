3c94bfeaa9cf4e9d7f9c07b7e65b35d702e87a99
Christopher Vogt <oss.nsp@cvogt.org>
2016-06-23 00:04:57 -0400

    add multi-project example

---
+++ examples/multi-project-example/build/build.scala
@@ -0,0 +1,10 @@
+import cbt._
+class Build(val context: Context) extends BaseBuild{
+  override def dependencies =
+    super.dependencies ++ // don't forget super.dependencies here
+    Seq(    
+      // source dependency
+      DirectoryDependency( projectDirectory ++ "/sub1" ),
+      DirectoryDependency( projectDirectory ++ "/sub2" )
+    )
+}
