9f538b65cc2d08e81d6bcbef26ce571b3b442882
Christopher Vogt <oss.nsp@cvogt.org>
2016-09-29 19:57:44 +0000

    Add most basic example

---
+++ examples/simple-example/build/build.scala
@@ -0,0 +1,20 @@
+import cbt._
+class Build(val context: Context) extends BaseBuild{
+  /*
+  override def dependencies =
+    super.dependencies ++ // don't forget super.dependencies here
+    Seq(    
+      // source dependency
+      DirectoryDependency( projectDirectory ++ "/subProject" )
+    ) ++
+    Resolver( mavenCentral ).bind(
+      // CBT-style Scala dependencies
+      ScalaDependency( "com.lihaoyi", "ammonite-ops", "0.5.5" )
+      MavenDependency( "com.lihaoyi", "ammonite-ops_2.11", "0.5.5" )
+
+      // SBT-style dependencies
+      "com.lihaoyi" %% "ammonite-ops" % "0.5.5"
+      "com.lihaoyi" % "ammonite-ops_2.11" % "0.5.5"
+    )
+  */
+}
