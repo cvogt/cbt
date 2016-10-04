908e05e296974fe67d8aaf9f094d97ff986905af
Christopher Vogt <oss.nsp@cvogt.org>
2016-10-03 21:39:15 -0400

    add sample library to test git dependencies

---
+++ test/library-test/build/build.scala
@@ -0,0 +1,21 @@
+import cbt._
+
+// cbt:https://github.com/cvogt/cbt.git#1f4f6097d3ca682d6fd20a7cc6dd277832350827
+class Build(val context: Context) extends BaseBuild{
+  override def dependencies =
+    super.dependencies ++ // don't forget super.dependencies here for scala-library, etc.
+    Seq(
+      // source dependency
+      // DirectoryDependency( projectDirectory ++ "/subProject" )
+    ) ++
+    // pick resolvers explicitly for individual dependencies (and their transitive dependencies)
+    Resolver( mavenCentral, sonatypeReleases ).bind(
+      // CBT-style Scala dependencies
+      // ScalaDependency( "com.lihaoyi", "ammonite-ops", "0.5.5" )
+      // MavenDependency( "com.lihaoyi", "ammonite-ops_2.11", "0.5.5" )
+
+      // SBT-style dependencies
+      // "com.lihaoyi" %% "ammonite-ops" % "0.5.5"
+      // "com.lihaoyi" % "ammonite-ops_2.11" % "0.5.5"
+    )
+}
