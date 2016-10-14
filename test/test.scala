9f0ce77f01c5bdd7cb4f475d3d526b8ac30f9cc4
Christopher Vogt <oss.nsp@cvogt.org>
2016-10-14 00:00:32 -0400

    add tests for cross-cbt-version exit code and err/out directing also adds example for propagating build-time information into run time

--- test/test.scala
+++ test/test.scala
@@ -212,16 +212,22 @@ object Main{
     compile("../examples/uber-jar-example")
     
     {
-      val res = task("docJar","library-test")
-      assert( res.out endsWith "library-test_2.11-0.1-javadoc.jar" )
-      assert( res.err startsWith "model contains" )
-      assert( res.err endsWith "documentable templates" )
+      val res = task("docJar","simple-fixed-cbt")
+      assert( res.out endsWith "simple-fixed-cbt_2.11-0.1-javadoc.jar", res.out )
+      assert( res.err contains "model contains", res.err )
+      assert( res.err endsWith "documentable templates", res.err )
     }
     
     {
       val res = runCbt("simple", Seq("printArgs","1","2","3"))
       assert(res.exit0)
       assert(res.out == "1 2 3", res.out)
+    }
+
+    {
+      val res = runCbt("../examples/build-info-example", Seq("run"))
+      assert(res.exit0)
+      assert(res.out contains "version: 0.1", res.out)
     }
 
     {
