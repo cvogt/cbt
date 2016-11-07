3949713f84b3a811646ae678e90bab257b4801a0
Christopher Vogt <oss.nsp@cvogt.org>
2016-11-07 02:53:42 -0500

    Add test for overriding Eval classloader

--- libraries/eval/test/EvalTest.scala
+++ libraries/eval/test/EvalTest.scala
@@ -252,6 +252,13 @@ class EvalTest extends WordSpec {
         eval.check("new A()")
         assert(eval.errors.isEmpty)
       }
+
+      "eval with overriden classloader" in {
+        val eval = new Eval{
+          override def classLoader = this.getClass.getClassLoader
+        }
+        assert(eval.apply[Int]("1 + 1") == 2)
+      }
     }
   }
 }
