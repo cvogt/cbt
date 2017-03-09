43424cd959e9f0c66788f035e05232ffc8057e4a
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-08 22:00:14 -0500

    minor improvements to sonatype messages

--- plugins/sonatype-release/src/sonatype/SonatypeLib.scala
+++ plugins/sonatype-release/src/sonatype/SonatypeLib.scala
@@ -54,7 +54,7 @@ final case class SonatypeLib(
   profileName: String,
   serviceURI: String = SonatypeLib.serviceURI,
   snapshotsURI: String = SonatypeLib.snapshotsURI,
-  credentials: String = SonatypeLib.credentials
+  credentials: String = SonatypeLib.credentials // FIXME: maybe hide this from cli ?
 )( implicit logger: Logger ){
   private val lib: Lib = new Lib(logger)
   private def log: String => Unit = logger.log("sonatype-release",_)
@@ -74,20 +74,21 @@ final case class SonatypeLib(
       artifacts, new URL(deployURI ++ releaseFolder), Some(credentials)
     )
 
-    val urls = if (releaseFolder.endsWith("-SNAPSHOT")){
-      publish(snapshotsURI)
+    if (releaseFolder.endsWith("-SNAPSHOT")){
+      val urls = publish(snapshotsURI)
+      System.err.println(lib.green("Successfully published snapshot on Sonatype!"))
+      urls
     } else {
       val profile = getStagingProfile
       val repoId = createStagingRepo(profile)
       val urls = publish(
         serviceURI ++ "/staging/deployByRepositoryId/" ++ repoId.string
       )
+      System.err.println(lib.green("Successfully uploaded jars to Sonatype!"))
       finishRelease( getStagingRepoById(repoId), profile )
-      urls 
+      System.err.println(lib.green("Successfully released uploaded jars to Maven Central!"))
+      urls
     }
-
-    System.err.println(lib.green("Successfully published to Sonatype!"))
-    urls
   }
 
   /*
@@ -100,6 +101,8 @@ final case class SonatypeLib(
     */
   private def release: ExitCode = {
     val profile = getStagingProfile()
+
+    System.err.println("Releasing jars to Maven Central!")
 
     getStagingRepos(profile).toList match {
       case Nil =>
