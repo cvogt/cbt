43951e14015339300df261d6798144dd22fe2098
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-19 19:54:56 -0400

    use standard cbt scala xml version

--- build/build.scala
+++ build/build.scala
@@ -11,7 +11,7 @@ class Build(val context: Context) extends Shared with PublishLocal{
   override def dependencies = {
     super.dependencies ++ Resolver(mavenCentral).bind(
       MavenDependency("org.eclipse.jgit", "org.eclipse.jgit", "4.2.0.201601211800-r"),
-      ScalaDependency("org.scala-lang.modules","scala-xml","1.0.5")
+      ScalaDependency("org.scala-lang.modules","scala-xml",constants.scalaXmlVersion)
     )
   } :+ libraries.eval
 
