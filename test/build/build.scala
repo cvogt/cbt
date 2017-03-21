fa17c4badb0af8abca8573fc71b683a6757af130
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-20 22:41:02 -0400

    allow running tests in the same process, forked or forked in direct mode

--- test/build/build.scala
+++ test/build/build.scala
@@ -3,5 +3,8 @@ import cbt._
 class Build(val context: cbt.Context) extends BaseBuild{
   override def dependencies = super.dependencies :+ context.cbtDependency
   def apply = run
+  override def run = {
+    classes.flatMap( lib.discoverCbtMain ).head( context )
+  }
   def args = context.args
 }
