d4797818603d64153da51adb19ef34a96b32d858
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-27 19:17:07 -0400

    give access to version specific classes in cross-build

--- stage2/plugins/MultipleScalaVersions.scala
+++ stage2/plugins/MultipleScalaVersions.scala
@@ -4,6 +4,8 @@ trait MultipleScalaVersions extends DynamicOverrides{
   def scalaVersions: Seq[String] = Seq(scalaVersion, "2.10.6")
   def cross: Seq[MultipleScalaVersions] =
     scalaVersions.map{ v =>
-      newBuild[MultipleScalaVersions](context.copy(scalaVersion = Some(v)))("")
+      newBuild[MultipleScalaVersions](context.copy(scalaVersion = Some(v)))("""
+        override def sources = super.sources :+ ( projectDirectory / "src_" ++ scalaMajorVersion )
+      """)
     }
 }
