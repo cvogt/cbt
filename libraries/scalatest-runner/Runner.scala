3e37741a3d937a9d4c6ce85691226ef1197ebab6
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-30 11:02:43 -0400

    ad cbt library build and scalariform to test scalatet-runner

--- libraries/scalatest-runner/Runner.scala
+++ libraries/scalatest-runner/Runner.scala
@@ -4,9 +4,9 @@ import org.scalatest._
 
 import java.io.File
 
-object Runner{
+object Runner {
   def run( classpath: Array[File], classLoader: ClassLoader ): Unit = {
-    val suiteNames = classpath.map( d => discoverSuites(d, classLoader) ).flatten
+    val suiteNames = classpath.map( d => discoverSuites( d, classLoader ) ).flatten
     runSuites( suiteNames.map( loadSuite( _, classLoader ) ) )
   }
 
@@ -18,20 +18,20 @@ object Runner{
     def stats: Boolean = true
     def testName: String = null
     def configMap: ConfigMap = ConfigMap.empty
-    suites.foreach{
-      _.execute(testName, configMap, color, durations, shortstacks, fullstacks, stats)
+    suites.foreach {
+      _.execute( testName, configMap, color, durations, shortstacks, fullstacks, stats )
     }
   }
 
   def discoverSuites( discoveryPath: File, classLoader: ClassLoader ): Seq[String] = {
     classLoader
-      .loadClass("org.scalatest.tools.SuiteDiscoveryHelper")
-      .getMethod("discoverSuiteNames", classOf[List[_]], classOf[ClassLoader], classOf[Option[_]])
-      .invoke(null, List(discoveryPath.toString ++ "/"), classLoader, None)
+      .loadClass( "org.scalatest.tools.SuiteDiscoveryHelper" )
+      .getMethod( "discoverSuiteNames", classOf[List[_]], classOf[ClassLoader], classOf[Option[_]] )
+      .invoke( null, List( discoveryPath.toString ++ "/" ), classLoader, None )
       .asInstanceOf[Set[String]]
       .toVector
   }
-  def loadSuite(name: String, classLoader: ClassLoader) = {
-    classLoader.loadClass(name).getConstructor().newInstance().asInstanceOf[Suite]
+  def loadSuite( name: String, classLoader: ClassLoader ) = {
+    classLoader.loadClass( name ).getConstructor().newInstance().asInstanceOf[Suite]
   }
 }
--- libraries/scalatest-runner/build/build.scala
+++ libraries/scalatest-runner/build/build.scala
@@ -1,6 +1,11 @@
+package cbt_libraries_build.scalatest_runner
 import cbt._
+import cbt_internal._
 
-class Build(val context: Context) extends BaseBuild{
+class Build(val context: Context) extends Library{
+  override def inceptionYear = 2017
+  override def description = "run scalatest tests from given directory and classpath (compatible with 2.12 and 2.11)"
+
   override def dependencies = super.dependencies ++
     Resolver( mavenCentral ).bind(
       ScalaDependency("org.scalatest","scalatest", if(scalaMajorVersion == "2.12") "3.0.1" else "2.2.6")
