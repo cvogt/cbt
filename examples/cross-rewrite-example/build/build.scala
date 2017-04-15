2251bd7cd997f1264178b1c678ba942ca0c78f65
Shane Delmore <shane@delmore.io>
2017-04-15 14:46:57 -0700

    Add example of semantic rewrite

--- examples/cross-rewrite-example/build/build.scala
+++ examples/cross-rewrite-example/build/build.scala
@@ -1,10 +1,14 @@
 package cbt_examples_build.cross_rewrite
 import cbt._
 import java.io.File
-import scala.meta._
+import scala.collection.immutable
+import scalafix.syntax._
+import scalafix.rewrite._
 import scalafix.util._
 import scalafix.util.TreePatch._
 import scalafix.util.TokenPatch._
+import scala.meta._
+import scala.meta.contrib._
 
 class TestBuild( val context: Context, mainBuild: BaseBuild ) extends ScalaTest with Scalameta {
   override def defaultScalaVersion = mainBuild.scalaVersion
@@ -22,8 +26,8 @@ class Build(val context: Context) extends BaseBuild with Scalameta with PackageJ
   override def test: BaseBuild = new TestBuild( context, this )
 
   def cross = for{
-    ( v, version_rewrites ) <- CrossRewrite.versions
-    ( label, dep, lib_rewrites ) <- CrossRewrite.libs
+    ( v, version_patches,  version_rewrites ) <- CrossRewrite.versions
+    ( label, dep, lib_patches, lib_rewrites ) <- CrossRewrite.libs
   } yield {
     new Build(context) with Scalafix{ patchedMainBuild =>
       override def defaultScalaVersion = v
@@ -44,6 +48,7 @@ class Build(val context: Context) extends BaseBuild with Scalameta with PackageJ
         defaultMainBuild.sources,
         projectDirectory / "src",
         defaultMainBuild.classpath,
+        lib_patches ++ version_patches,
         lib_rewrites ++ version_rewrites,
         lib
       )
@@ -53,6 +58,7 @@ class Build(val context: Context) extends BaseBuild with Scalameta with PackageJ
           defaultMainBuild.test.sources,
           projectDirectory / "src",
           defaultMainBuild.test.classpath,
+          lib_patches ++ version_patches,
           lib_rewrites ++ version_rewrites,
           lib
         )
@@ -62,18 +68,11 @@ class Build(val context: Context) extends BaseBuild with Scalameta with PackageJ
 }
 
 object CrossRewrite{
-  def versions = Seq[(String, Seq[Patch])](
-    "2.12.1" -> Seq(),
-    "2.11.8" -> Seq(
-      RemoveGlobalImport(
-        importer"scala.concurrent.Future"
-      ),
-      AddGlobalImport(
-        importer"scala.util.Try"
-      )
-    )
+  def versions = Seq[(String, Seq[Patch], Seq[Rewrite[ScalafixMirror]])](
+    ("2.12.1", Seq(), Seq()),
+    ("2.11.8", Seq(), Seq())
   )
-  def libs = Seq[(String, MavenDependency, Seq[Patch])](
+  def libs = Seq[(String, MavenDependency, Seq[Patch], Seq[Rewrite[ScalafixMirror]])](
     (
       "scalaz",
       MavenDependency( "org.scalaz", "scalaz-core", "7.2.10" ),
@@ -82,16 +81,29 @@ object CrossRewrite{
         Replace(Symbol("_root_.scala.package.Either."), q"\/"),
         Replace(Symbol("_root_.scala.util.Right."), q"\/-"),
         RemoveGlobalImport(importer"cats.implicits._")
-      )
+      ),
+      Seq()
     ),
     (
       "cats",
       MavenDependency( "org.typelevel", "cats", "0.9.0" ),
-      Seq(
-        AddGlobalImport(importer"cats.implicits._")
-      )
+      Seq(),
+      Seq(addCatsImplicitsIfNeeded)
     )
   )
+
+  //This is an example of adding inmport only if a specific method is used in the source file.
+  val addCatsImplicitsIfNeeded = Rewrite[ScalafixMirror] { ctx =>
+    implicit val mirror = ctx.mirror //ctx.tree methods using the semantic mirror rely on an implicit mirror in scope
+    val symbolForEitherMap = Symbol("_root_.scala.util.Either.map.") //The symbol we are looking for in the file
+    val symbolFoundInCurrentFile = ctx.tree.exists {
+          case t: Term.Name //Use the semantic mirror to compare symbols in the file with the symbol we are looking for
+            if mirror.symbol(t).toOption.exists(_.normalized == symbolForEitherMap) => true
+          case _ => false
+        }
+    //Only add the import to files containing the symbol we are looking for, Either.map in this case.
+    immutable.Seq(AddGlobalImport(importer"cats.implicits._")).filter(_ => symbolFoundInCurrentFile)
+  }
 
   def mkdirIfNotExists( d: File ): File = {
     d.mkdirs
@@ -103,6 +115,7 @@ object CrossRewrite{
     destination: File,
     semanticDbClassPath: ClassPath,
     patches: Seq[Patch],
+    rewrites: Seq[Rewrite[ScalafixMirror]],
     lib: Lib
   ) = {
     val fromTo = lib.autoRelative( sources ).collect{
@@ -117,6 +130,7 @@ object CrossRewrite{
       semanticDbClassPath,
       files = fromTo,
       patches = patches,
+      rewrites = rewrites,
       allowEmpty = true
     ).apply
 
