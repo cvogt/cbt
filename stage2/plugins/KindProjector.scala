07bd7bef8f811c03f133f42bb56a3076cc6ccb14
Jan Christopher Vogt <oss.nsp@cvogt.org>
2017-06-15 16:23:25 -0400

    adjusted whitespace to cbt convention

--- stage2/plugins/KindProjector.scala
+++ stage2/plugins/KindProjector.scala
@@ -13,10 +13,10 @@ trait KindProjectorPlugin extends BaseBuild {
 
 object KindProjector {
   def dependencies(
-                    scalaMajorVersion: String, kindProjectorVersion: String, cbtLastModified: Long, mavenCache: File
-                  )(
-                    implicit logger: Logger, transientCache: java.util.Map[AnyRef,AnyRef], classLoaderCache: ClassLoaderCache
-                  ) =
+    scalaMajorVersion: String, kindProjectorVersion: String, cbtLastModified: Long, mavenCache: File
+  )(
+    implicit logger: Logger, transientCache: java.util.Map[AnyRef,AnyRef], classLoaderCache: ClassLoaderCache
+  ) =
     MavenResolver( cbtLastModified, mavenCache, mavenCentral ).bindOne(
       MavenDependency(
         "org.spire-math", "kind-projector_"+scalaMajorVersion, kindProjectorVersion
