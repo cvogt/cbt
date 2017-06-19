e602de98199e48669a7fcd7c70df6f0f1c2e841a
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-06-19 19:20:14 +0300

    Error fixes

--- stage2/plugins/ExportBuildInformation.scala
+++ stage2/plugins/ExportBuildInformation.scala
@@ -2,15 +2,15 @@ package cbt
 
 import cbt._
 import scala.xml._
+import scala.util._
 import java.io._
+import scala.collection.JavaConverters._
 
 trait ExportBuildInformation { self: BaseBuild =>
   lazy val printer = new scala.xml.PrettyPrinter(200, 2)
 
   def buildInfoXml = 
     printer.format(BuildInformationSerializer.serialize(BuildInformation.Project(self)))
-
-  def a = DirectoryDependency(context.cbtHome).dependenciesArray.map(_.toString).mkString("\n")
 }
 
 object BuildInformation {
@@ -46,21 +46,14 @@ object BuildInformation {
     def apply(build: BaseBuild) =   
      new BuildInformationExporter(build).exportBuildInformation  
 
-    private class BuildInformationExporter(rootBuild: BaseBuild) {
+    class BuildInformationExporter(rootBuild: BaseBuild) {
       def exportBuildInformation(): Project = {
-        val moduleBuilds = collectBuildls(rootBuild)
+        val moduleBuilds = transitiveBuilds(rootBuild)
         val libraries = moduleBuilds
           .flatMap(_.transitiveDependencies)
           .collect { case d: BoundMavenDependency => exportLibrary(d)}
           .distinct
-        val cbtLibraries = 
-          collectBuildls(DirectoryDependency(rootBuild.context.cbtHome)(rootBuild.context).dependenciesArray.head.asInstanceOf[BaseBuild])
-            .collect {
-              case d: BoundMavenDependency => d.jar
-              case d: PackageJars => d.jar.get
-            }
-          .map(exportLibrary)
-          .distinct
+        val cbtLibraries = convertCbtLibraries
         val cbtLibraryDependencies = cbtLibraries
           .map(l => LibraryDependency(l.name))
         val rootModule = exportModule(cbtLibraryDependencies)(rootBuild)
@@ -74,11 +67,40 @@ object BuildInformation {
         )
       }
 
-      def collectBuildls(build: BaseBuild) = 
+      private def convertCbtLibraries() = 
+        transitiveBuilds(DirectoryDependency(rootBuild.context.cbtHome)(rootBuild.context).dependenciesArray.head.asInstanceOf[BaseBuild])
+        .collect {
+          case d: BoundMavenDependency => d.jar
+          case d: PackageJars => d.jar.get
+        }
+        .map(exportLibrary)
+        .distinct
+
+          
+      private def collectLazyBuilds(dependency: Dependency): Option[BaseBuild] = 
+        dependency match {
+          case l: LazyDependency =>         
+            l.dependency match {
+               case d: BaseBuild => Some(d)
+               case d: LazyDependency => collectLazyBuilds(d.dependency)
+               case _ => None
+            }
+          case d: BaseBuild => Some(d)
+          case _ => None
+        }
+
+
+      private def transitiveBuilds(build: BaseBuild): Seq[BaseBuild] =             
         (build +: build.transitiveDependencies)
-          .collect { case d: BaseBuild => d +: collectParentBuilds(d)}
-          .flatten
-          .distinct
+        .collect {
+          case d: BaseBuild => d +: collectParentBuilds(d).flatMap(transitiveBuilds)
+          case d: LazyDependency => 
+            collectLazyBuilds(d.dependency)
+              .toSeq
+              .flatMap(transitiveBuilds)
+        }
+        .flatten
+        .distinct
 
       private def exportLibrary(mavenDependency: BoundMavenDependency) = 
         Library(fomatMavenDependency(mavenDependency.mavenDependency), mavenDependency.exportedJars)
@@ -93,22 +115,23 @@ object BuildInformation {
             .toSeq
             .flatten
 
-      private def exportModule(cbtLibraryDependencies: Seq[LibraryDependency])(build: BaseBuild): Module = {       
-        val moduleDependencies = build.dependencies
-          .collect { case d: BaseBuild => ModuleDependency(moduleName(d)) }
+      private def exportModule(cbtLibraryDependencies: Seq[LibraryDependency])(build: BaseBuild): Module = {
+        def collectDependencies(dependencies: Seq[Dependency]): Seq[ModuleDependency] = 
+          dependencies
+          .collect {
+             case d: BaseBuild => Seq(ModuleDependency(moduleName(d)))
+             case d: LazyDependency => collectDependencies(Seq(d.dependency))
+           }
+          .flatten      
+        val moduleDependencies = collectDependencies(build.dependencies)
         val mavenDependencies = build.dependencies
           .collect { case d: BoundMavenDependency => LibraryDependency(fomatMavenDependency(d.mavenDependency))}
         val classpaths = build.dependencyClasspath.files
           .filter(_.isFile)
           .map(t => ClassPathItem(t))
-        val sources = {
-          val s = build.sources
-            .filter(s => s.exists && s.isDirectory)
-          if (s.isEmpty)
-            Seq(build.projectDirectory)
-          else
-            s
-        }
+        val sources = build.sources
+          .filter(s => s.exists && s.isDirectory) :+ build.projectDirectory       
+        
         Module(
           name = moduleName(build),
           root = build.projectDirectory,
