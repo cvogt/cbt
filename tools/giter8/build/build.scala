d65fa467f072435b1631391994c0f8eb547446eb
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-07-14 16:22:03 +0300

    Add arguments parsing

--- tools/giter8/build/build.scala
+++ tools/giter8/build/build.scala
@@ -6,12 +6,26 @@ import giter8._
 class Build(val context: Context) extends BaseBuild {
   def createTemplate: String = {
     val helper = new JgitHelper(new Git(new JGitInteractor), G8TemplateRenderer)
-    val (dir, args) = context.args.toList match {
-      case Nil => (context.cwd, Seq.empty)
-      case x::Nil if x.startsWith("--") => (context.cwd, Seq(x))
-      case x :: xs => (new File(x), xs)
-    }
-    val config = Config("scala/scala-seed.g8")
-    helper.run(config, args, dir).toString
+    val result =
+      (context.args.toList match {
+        case t :: Nil => Right(t, context.cwd, Seq.empty)
+        case t :: x :: Nil if x.startsWith("--") => Right(t, context.cwd, Seq(x))
+        case t :: x :: xs => Right(t, new File(x), xs)
+        case Nil => Left("Please, specify a template name")
+      }).right.flatMap { case (template, dir, args) =>
+        Either.cond(args.forall(validateArgument),
+          (template, dir, args),
+          """Incorrect arguments, it should be --<name>=<value>
+            |Example --name=my_cool_project
+          """.stripMargin)
+      }.right.flatMap { case (template, dir, args) =>
+        val config = Config(template)
+        helper.run(config, args, dir)
+      }
+
+    result.fold(identity, identity)
   }
+
+  private def validateArgument(arg: String) =
+    arg.matches("""--[\w]+=[\w]+""")
 }
