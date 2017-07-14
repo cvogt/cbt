5019c28a9daba6cbfc10a55340aee3cc824a7f5a
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-07-14 15:42:34 +0300

    Basic arguments parsing

--- tools/giter8/build/build.scala
+++ tools/giter8/build/build.scala
@@ -6,8 +6,12 @@ import giter8._
 class Build(val context: Context) extends BaseBuild {
   def createTemplate: String = {
     val helper = new JgitHelper(new Git(new JGitInteractor), G8TemplateRenderer)
-    val dir = new File(context.args.head)
+    val (dir, args) = context.args.toList match {
+      case Nil => (context.cwd, Seq.empty)
+      case x::Nil if x.startsWith("--") => (context.cwd, Seq(x))
+      case x :: xs => (new File(x), xs)
+    }
     val config = Config("scala/scala-seed.g8")
-    helper.run(config, Seq.empty, dir).toString
+    helper.run(config, args, dir).toString
   }
 }
